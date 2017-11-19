5dced7b3a4edf1da88e49194b877d6965e2ae2f3
vladglushko1006 <vladglushko1006@gmail.com>
2017-11-19 19:40:02 +0200

    Add types

--- CharityApp/CharityApp/Data/Migrations/20171119173823_Types.Designer.cs
+++ CharityApp/CharityApp/Data/Migrations/20171119173823_Types.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharityApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171119173823_Types")]
+    partial class Types
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
